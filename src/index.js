472d963f4f72d1e3768b0ef52e47f0fd8cc48991
pashamakhilkumarreddy <akhil.reddy@frequence.com>
2020-05-06 00:19:14 +0530

    minor fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './assets/css/main.css';
+import './index.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
